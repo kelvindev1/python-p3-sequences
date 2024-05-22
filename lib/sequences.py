39bb87b3bb41879d4ea74cb7e03ea0f1c067a2b6
kelvin <kelvinmutugi336@gmil.com>
2024-05-22 22:29:03 +0300

    solves lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    elif length == 2:
+        print("[0, 1]")
+    else:
+        fib_sequence = [0, 1]
+        for _ in range(2, length):
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        print(fib_sequence)
+
+print("Fibonacci Series - ")
+print_fibonacci(0)
+print_fibonacci(1)
+print_fibonacci(2)
+print_fibonacci(10)
